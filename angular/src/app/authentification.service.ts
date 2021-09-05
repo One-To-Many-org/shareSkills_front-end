2f4ffbdec264d0a19cb7e6580867dda39d7c4f88
Aremou <faizouaremou@gmail.com>
2021-09-05 20:55:24 +0200

    delete webteamtimes library from project

--- angular/src/app/authentification.service.ts
+++ angular/src/app/authentification.service.ts
@@ -4,7 +4,6 @@ import { Observable, throwError } from 'rxjs';
 import { catchError, map, retry, tap } from 'rxjs/operators';
 import { User } from './class/types';
 import { environment } from 'src/environments/environment';
-import { HttpErrorHandlerService , HandleError } from '@web-times-team/angular-web-times-tools';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -19,17 +18,15 @@ const httpOptions = {
 })
 
 export class AuthentificationService {
-  private handleError: HandleError;
   isLoggedIn = false;
   // store the URL so we can redirect after logging in
   redirectUrl: string;
-  constructor(private http: HttpClient,  httpErrorHandler: HttpErrorHandlerService) {
-    this.handleError = httpErrorHandler.createHandleError('AuthentificationService');
-   }
+  constructor(private http: HttpClient) {}
 
-  authentificationUrl = environment.apiUrl + 'login-page/login';
-  isAuthUrl = environment.apiUrl + 'login-page/is-auth';
-  logoutUrl = environment.apiUrl + 'login-page/logout';
+
+  private authentificationUrl = environment.apiUrl + 'login-page/login';
+  private isAuthUrl = environment.apiUrl + 'login-page/is-auth';
+  private logoutUrl = environment.apiUrl + 'login-page/logout';
   /**
    * login user
    */
@@ -43,8 +40,7 @@ export class AuthentificationService {
       .pipe(
         tap(res => {
           this.isLoggedIn = true;
-        }),
-        catchError(this.handleError('login', false))
+        })
       )
   }
   /**
@@ -55,8 +51,7 @@ export class AuthentificationService {
       map(res => {
         this.isLoggedIn = res.authenticated;
         return this.isLoggedIn;
-      }),
-      catchError(this.handleError('login', true))
+      })
     );
   }
   /**
@@ -67,8 +62,7 @@ export class AuthentificationService {
       map(res => {
         this.isLoggedIn = res.authenticated;
         return this.isLoggedIn;
-      }),
-      catchError(this.handleError('isAuth', false))
+      })
     );
   }
 }
--- angular/src/app/login-page/login-page.component.ts
+++ angular/src/app/login-page/login-page.component.ts
@@ -1,36 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { MediaButInfType, UsernameField } from '@web-times-team/angular-web-times-tools';
-import { AuthentificationService } from '../authentification.service';
-
-
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
-  mediasButInfTypes: MediaButInfType[] = [MediaButInfType.facebook, MediaButInfType.google, MediaButInfType.linkedIn];
-  usernameField = UsernameField[1];
-  forgotPasswordRoute = '';
-  signupRoute = 'registration'
-  message: string;
-  constructor(private authentification: AuthentificationService, private router: Router) { }
+
+
+  constructor() { }
 
   ngOnInit(): void {
-  }
-  setMessage() {
-    this.message = 'Logged ' + (this.authentification.isLoggedIn ? 'in' : 'out');
-  }
-  /**
-   * authentifie users
-   */
-  login(credentials: any): void {
-    this.authentification.login(credentials).subscribe(
-      user => {
-        this.router.navigate(['collaborations']);
-      },
-    )
   }
 
 }
