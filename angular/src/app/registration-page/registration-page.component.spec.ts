5d58af14462ad33c5986704081afb0840f03a2be
Aremou <faizouaremou@gmail.com>
2021-09-05 20:38:03 +0200

    install angular 12

--- angular/src/app/registration-page/registration-page.component.spec.ts
+++ angular/src/app/registration-page/registration-page.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { RegistrationPageComponent } from './registration-page.component';
 
@@ -6,7 +6,7 @@ describe('RegistrationPageComponent', () => {
   let component: RegistrationPageComponent;
   let fixture: ComponentFixture<RegistrationPageComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ RegistrationPageComponent ]
     })
